b8492ff0859679ca2546f240c0399a25665eb99e
Lucas Colucci <lucas.colucci@plataformatec.com.br>
2017-11-30 11:24:48 -0200

    fixing

--- run.R
+++ run.R
@@ -3,29 +3,8 @@ library(routr)
 library(lpSolveAPI)
 library(lpSolve)
 
-set.seed(1492)
-x <- rnorm(15)
-y <- x + rnorm(15)
-fit <- lm(y ~ x)
-saveRDS(fit, "model.rds")
-
 app <- Fire$new(host = '0.0.0.0', port = as.integer(Sys.getenv('PORT')))
 app$set_logger(logger_console())
-
-# When the app starts, we'll load the model we saved. Instead of
-# polluting our namespace we'll use the internal data store
-
-app$on('start', function(server, ...) {
-  server$set_data('model', readRDS('model.rds'))
-  message('Model loaded')
-})
-
-# Just for show off, we'll make it so that the model is automatically
-# passed on to the request handlers
-
-app$on('before-request', function(server, ...) {
-  list(model = server$get_data('model'))
-})
 
 # Now comes the biggest deviation. We'll use routr to define our request
 # logic, as this is much nicer
@@ -40,26 +19,9 @@ route$add_handler('get', '*', function(request, response, keys, ...) {
   response$body <- '<h1>All your AI are belong to us</h1>'
   TRUE
 })
-# Then on to the /info route
-route$add_handler('get', '/info', function(request, response, keys, ...) {
-  response$status <- 200L
-  response$body <- structure(R.Version(), class = 'list')
-  response$format(json = reqres::format_json())
-  TRUE
-})
-# Lastly we add the /predict route
-route$add_handler('get', '/predict', function(request, response, keys, arg_list, ...) {
-  response$body <- predict(
-    arg_list$model, 
-    data.frame(x=as.numeric(request$query$val)),
-    se.fit = TRUE
-  )
-  response$status <- 200L
-  response$format(json = reqres::format_json())
-  TRUE
-})
-# Lastly we add the /schedule route
-route$add_handler('get', '/schedule', function(request, response, keys, ...) {
+
+#add the /schedulev1 route
+route$add_handler('get', '/schedulev1', function(request, response, keys, ...) {
   dir <- as.numeric(unlist(strsplit(request$query$dir, "\\,")))
   dir <- replace( dir, dir==0, "=")
   dir <- replace( dir, dir==1, ">")
@@ -72,12 +34,6 @@ route$add_handler('get', '/schedule', function(request, response, keys, ...) {
   number_of_solutions <- as.numeric(request$query$number_of_solutions, "\\,")
   con <- as.numeric(unlist(strsplit(request$query$con, "\\,")))
   con <- matrix(con, nrow=length(dir), byrow=TRUE)
-  
-    # response$body <- c(obj,
-    #                  dir,
-    #                  rhs,
-    #                  number_of_solutions,
-    #                  con)
     
   response$body <-lp("max",
              obj,
@@ -90,15 +46,31 @@ route$add_handler('get', '/schedule', function(request, response, keys, ...) {
   response$format(json = reqres::format_json())
   TRUE
 })
-# And just to show off reqres file handling, we'll add a route 
-# for getting a model plot
-route$add_handler('get', '/plot', function(request, response, keys, arg_list, ...) {
-  f_path <- tempfile(fileext = '.png')
-  png(f_path)
-  plot(arg_list$model)
-  dev.off()
+
+# add the /schedulev2 route
+route$add_handler('get', '/schedulev2', function(request, response, keys, ...) {
+  dir <- as.numeric(unlist(strsplit(toString(Sys.getenv("")), "\\,")))
+  dir <- replace( dir, dir==0, "=")
+  dir <- replace( dir, dir==1, ">")
+  dir <- replace( dir, dir==2, "<")
+  dir <- replace( dir, dir==3, ">=")
+  dir <- replace( dir, dir==4, "<=")
+  
+  obj <- as.numeric(unlist(strsplit(toString(Sys.getenv("")), "\\,")))
+  rhs <- as.numeric(unlist(strsplit(toString(Sys.getenv("")), "\\,")))
+  number_of_solutions <- as.numeric(toString(Sys.getenv("")), "\\,")
+  con <- as.numeric(unlist(strsplit(toString(Sys.getenv("")), "\\,")))
+  con <- matrix(con, nrow=length(dir), byrow=TRUE)
+  
+  response$body <-lp("max",
+                     obj,
+                     con,
+                     dir,
+                     rhs,
+                     all.bin=TRUE,
+                     num.bin.solns=number_of_solutions)$solution
   response$status <- 200L
-  response$attach(f_path, filename = 'model_plot.png')
+  response$format(json = reqres::format_json())
   TRUE
 })
 
--- scheduler.r
+++ scheduler.r
@@ -1,14 +1,14 @@
 library(lpSolveAPI)
 library(lpSolve)
 
-number_of_people = 8
-number_of_projects = 4 
+number_of_people = 15
+number_of_projects = 7 
 f_i_j <- c(matrix(t(read.csv(file="Documents/Alocacao/F(i,j).csv", header=TRUE, sep=",", colClasses = c(rep("NULL", 1), rep("integer", number_of_projects))))))
 f.obj <- f_i_j
 one_project_per_person_constraint <- c(rep(0,number_of_people*number_of_people*number_of_projects))
 for (i in 0:(number_of_people-1)) {
   for (j in 1:number_of_projects) {
-    one_project_per_person_constraint[j+i*4+i*number_of_people*number_of_projects] = 1 
+    one_project_per_person_constraint[j+i*number_of_projects+i*number_of_people*number_of_projects] = 1 
   }
 }
 
@@ -16,14 +16,14 @@ senior_constraint <- c(rep(0,number_of_projects*number_of_people*number_of_proje
 qi <- c(matrix(t(read.csv(file="Documents/Alocacao/qi.csv", header=TRUE, sep=",", colClasses = c(rep("NULL", 1), rep("integer", 1))))))
 for (i in 0:(number_of_projects-1)) {
   for(j in 0:(number_of_people-1)){
-    senior_constraint[(1+3*j)+j+i+i*number_of_people*number_of_projects] = qi[j+1]
+    senior_constraint[(1+(number_of_projects-1)*j)+j+i+i*number_of_people*number_of_projects] = qi[j+1]
   }
 }
 
 number_of_people_per_project_contraint <- c(rep(0,number_of_projects*number_of_people*number_of_projects))
 for (i in 0:(number_of_projects-1)) {
   for(j in 0:(number_of_people-1)){
-    number_of_people_per_project_contraint[(1+3*j)+j+i+i*number_of_people*number_of_projects] = 1
+    number_of_people_per_project_contraint[(1+(number_of_projects-1)*j)+j+i+i*number_of_people*number_of_projects] = 1
   }
 }
 
@@ -32,7 +32,7 @@ f.con <- matrix(
                 c(one_project_per_person_constraint, 
                   senior_constraint, 
                   number_of_people_per_project_contraint),
-                nrow=16, byrow=TRUE)
+                nrow=(number_of_people+number_of_projects+number_of_projects), byrow=TRUE)
 
 symbols <- c(rep("=", number_of_people), 
              rep(">=", number_of_projects), 
@@ -46,6 +46,6 @@ rhs <- c(rep(1, number_of_people),
          Nj)
 f.rhs <- rhs
 
-number_of_solutions = 5
+number_of_solutions = 1
 result <- lp("max", f.obj, f.con, f.dir, f.rhs,all.bin=TRUE,num.bin.solns=number_of_solutions)$solution
 write.csv(t(matrix(result[1:(number_of_people*number_of_projects*number_of_solutions)],nrow = number_of_projects,ncol = number_of_people*number_of_solutions)),file="Documents/Alocacao/solution.csv")
